dc3f1827ea2ddbe3ccd2036da4850b3f7315ca03
Bassel Al Omari <b2alomar@uwaterloo.ca>
2023-02-05 03:42:40 +0800

    feat: split training params

---
+++ optimizer_params.py
@@ -0,0 +1,21 @@
+class PaperParams:
+    def __init__(self):
+        self.epochs = 3000
+        self.lr = 10
+        self.momentum = 0.5
+    
+    def optimizer_update_fn(self, optimizer, epoch):
+        optimizer.param_groups[0]["lr"] *= 0.998
+        if epoch < 500:
+            optimizer.param_groups[0]["momentum"] += (0.99 - 0.5) / 500
+
+class LocalParams:
+    def __init__(self):
+        self.epochs = 30
+        self.lr = 0.1
+        self.momentum = 0.5
+    
+    def optimizer_update_fn(self, optimizer, epoch):
+        optimizer.param_groups[0]["lr"] *= 0.95
+        if epoch < 5:
+            optimizer.param_groups[0]["momentum"] += (0.9 - 0.5) / 5
