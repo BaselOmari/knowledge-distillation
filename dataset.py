33e8ae110ffae28941b0ad22bff17f1052266f56
Bassel Al Omari <b2alomar@uwaterloo.ca>
2023-02-05 04:42:48 +0800

    fix: normalized input dataset

--- dataset.py
+++ dataset.py
@@ -5,21 +5,24 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 
 
-def load_mnist(isTrainSet=True, withJitter=False):
-    transform_list = [torchvision.transforms.ToTensor()]
-    if withJitter:
+def load_mnist(is_train_set=True, with_jitter=False):
+    transform_list = [
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.1307,), (0.3081,))
+    ]
+    if with_jitter:
         transform_list.append(torchvision.transforms.GaussianBlur(3, 0.5))
     dset = datasets.MNIST(
         os.path.dirname(os.path.realpath(__file__)) + "/data",
-        train=isTrainSet,
+        train=is_train_set,
         download=True,
         transform=torchvision.transforms.Compose(transform_list),
     )
     return dset
 
 
-def get_dataloader(dataset, batchSize=100):
+def get_dataloader(dataset, batch_size=100):
     dataloader = DataLoader(
-        dataset=dataset, batch_size=batchSize, shuffle=True, drop_last=True
+        dataset=dataset, batch_size=batch_size, shuffle=True, drop_last=True
     )
     return dataloader
